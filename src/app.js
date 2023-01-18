9ea6a146add2151147ff9eb08b391ee80989ffa0
Alex Tom <tomaszewska.alexandra@gmail.com>
2023-01-18 11:22:04 +0000

    added js time

--- src/app.js
+++ src/app.js
@@ -1,22 +1,56 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    let day = days[date.getDay()];
+    return `${day}, ${hours}:${minutes}`;
+  }
+
+
 function displayWeather(response) {
   console.log(response.data);
-document.querySelector("#city").innerHTML = response.data.name;
-document.querySelector("#current-temp").innerHTML = Math.round(response.data.main.temp)
-document.querySelector("#description").innerHTML = response.data.weather[0].description;
-document.querySelector("#feels_like").innerHTML = Math.round(response.data.main.feels_like);
-document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-document.querySelector("#pressure").innerHTML = response.data.main.pressure;
-document.querySelector("#wind-speed").innerHTML = Math.round(response.data.wind.speed);
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#current-temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#feels_like").innerHTML = Math.round(
+    response.data.main.feels_like
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#pressure").innerHTML = response.data.main.pressure;
+  document.querySelector("#wind-speed").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#date").innerHTML = formatDate(
+    response.data.dt * 1000
+  );
 }
-
 
 // let apiKey = "d3404e661974cfd3od9d68t5333a8f2b";
 // let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=Lodz&key=${apiKey}'
-
 
 let apiKey = "41f9f6ba4afb61d172bc15ed2c8d65a6";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lodz&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
 axios.get(apiUrl).then(displayWeather);
-  
